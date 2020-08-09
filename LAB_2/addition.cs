1ac2beb742a9fc1b1e77c2eea9d9bcdb71aca8dd
Mauneel Sorathia <62179014+mauneel33@users.noreply.github.com>
2020-08-09 14:39:25 +0530

    Add files via upload

---
+++ LAB_2/addition.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace calc
+{
+	public class Addition
+	{
+		public int add(int x, int y)
+		{
+			return x+y;
+		}
+	}
+}
---
+++ LAB_2/multiplication.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace calc
+{
+	public class Multiplication
+	{
+		public int mult(int x, int y)
+		{
+			return x*y;
+		}
+	}
+}
